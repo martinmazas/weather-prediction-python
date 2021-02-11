26898a45aaefc7c1205f958faffe2f4604a636d8
martinmazas <mazasmartin@gmail.com>
2021-02-11 10:32:04 +0200

    working on task 2

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ def categorical_feature_to_numerical(df, feature):
 def main():
     # Create data set
     weather_df = pd.read_csv('weather3.csv')
+    weather_df['RainToday'].replace({'No': 0, 'Yes': 1}, inplace=True)
+    weather_df['RainTomorrow'].replace({'No': 0, 'Yes': 1}, inplace=True)
+    sns.pairplot(weather_df[['Humidity3pm', 'Humidity9am', 'RainToday', 'RainTomorrow']], hue='RainTomorrow', height=2.5)
+    sns.pairplot(weather_df[['Pressure9am', 'Pressure3pm', 'Temp3pm', 'RainTomorrow']], hue='RainTomorrow',
+                 height=2.5)
+    plt.show()
     # print(weather_df.info())
     # columns = weather_df.columns
     # print(columns)
@@ -32,12 +38,13 @@ def main():
     """Check how many values are in each column. 
     There are some columns with less than 60% of the data, so ignore them"""
     # print(weather_df.count().sort_values())
-    weather_df = weather_df.drop(columns=['Sunshine', 'Evaporation', 'Cloud3pm', 'Cloud9am'], axis=1)
-    # print(weather_df.shape)
+    # weather_df = weather_df.drop(columns=['Sunshine', 'Evaporation', 'Cloud3pm', 'Cloud9am'], axis=1)
+    # # print(weather_df.shape)
+    #
+    # """There are some columns that still have nan values, so ignore them to"""
+    # weather_df = weather_df.dropna()
+    # print(weather_df.count().sort_values())
 
-    """There are some columns that still have nan values, so ignore them to"""
-    weather_df = weather_df.dropna()
-    print(weather_df.count().sort_values())
 
     # categorical_features = ['Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm', 'RainToday', 'RainTomorrow']
     # weather_df.dropna(inplace=True, subset=categorical_features)
